e7cb86eeb10b5cb5b82c60924482f02d60fc30ff
Benjamin Rosas <ben@aliencyb.org>
2020-07-01 17:26:04 -0500

    useEffect for rendering polis script

--- src/pages/conversations.js
+++ src/pages/conversations.js
@@ -4,19 +4,28 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import useScript from "../hooks/use-script"
 
-const ConversationsPage = ({ data: { strapiConversations } }) => (
-  <Layout>
-    <SEO title={strapiConversations.title} />
-    <h1>{strapiConversations.title}</h1>
-    <h2>{strapiConversations.subtitle}</h2>
-    {strapiConversations.content.map(contentblock => (
-      <React.Fragment key={contentblock.id}>
-        <ReactMarkdown source={contentblock.text} />
-      </React.Fragment>
-    ))}
-  </Layout>
-)
+const ConversationsPage = ({ data: { strapiConversations } }) => {
+  useScript("https://pol.is/embed.js")
+  return (
+    <Layout>
+      <SEO title={strapiConversations.title} />
+      <h1>{strapiConversations.title}</h1>
+      <h2>{strapiConversations.subtitle}</h2>
+      {strapiConversations.content.map(contentblock => (
+        <React.Fragment key={contentblock.id}>
+          <ReactMarkdown source={contentblock.text} />
+        </React.Fragment>
+      ))}
+      <div
+        className="polis"
+        data-page_id="PAGE_ID"
+        data-site_id="polis_site_id_fXsDvPcuW1QhMPz2oV"
+      ></div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query ConversationsPageQuery {
