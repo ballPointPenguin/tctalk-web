baed10a2fce1204accf0acc4f0003c2f09b0450a
Benjamin Rosas <ben@aliencyb.org>
2020-06-30 19:16:43 -0500

    add my icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,10 +25,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#9fb4cc`,
+        theme_color: `#5483b9`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tc-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
