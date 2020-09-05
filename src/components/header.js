bce6b37aee21c0a151ec733f9622fd7a779262d2
Benjamin Rosas <ben@aliencyb.org>
2020-09-05 18:24:25 -0500

    quick and dirty nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,47 @@
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import tw from 'twin.macro'
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div tw="text-center">
-      <h3 tw="bg-purple-200 border-b border-indigo-400">
+const Header = ({ siteTitle }) => {
+  const {
+    strapi: { pages = [] },
+  } = useStaticQuery(query)
+  return (
+    <header
+      tw="bg-purple-200 border-b border-indigo-400 text-base sm:text-2xl
+          flex flex-col sm:flex-row justify-around text-indigo-800"
+    >
+      <span>
         <Link to="/">{siteTitle}</Link>
-      </h3>
-    </div>
-  </header>
-)
+      </span>
+      {pages.map(page => (
+        <span key={page.id}>
+          <Link to={`/${page.slug}`}>{page.title}</Link>
+        </span>
+      ))}
+    </header>
+  )
+}
+
+export const query = graphql`
+  query HeaderQuery {
+    strapi {
+      pages(where: { nav_menu: true }) {
+        id
+        slug
+        title
+      }
+    }
+  }
+`
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Twin Cities Talk`,
 }
 
 export default Header
